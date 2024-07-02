115092d03bb5f570a198bbd1df1fa9b4256ae0a4
Mayank Gupta <mayankgupta14042000@gmail.com>
2024-07-02 23:10:08 +0530

    payload size

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({limit: '100mb'}));
 
 app.post('/clickup', sendToClickup);
 
